1228fdc0fe788673e34600ec01c4b14dd569adfa
Miguel Chichorro de Oliveira <51164882+MiguelChichorro@users.noreply.github.com>
2021-10-03 15:38:17 -0300

    Ex 3 Done

---
+++ Programming Language/list 3/Exercise 3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+
+int main (void){
+	int n,res,soma;
+	printf("Numero? ");
+	scanf("%d", &n);
+    
+	res = 1;
+	soma = res;
+	printf("\n%d",res);
+	for (int i = 1; i < n ;i++){
+		res = res + 2;
+		soma = soma + res;
+		printf ("+%d",res);
+	}
+	printf("\nQuadrado de %d = %2d\n",n,soma);
+	return 0;
+}
