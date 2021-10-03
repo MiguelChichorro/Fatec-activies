9bfa745b8210aaf57f5f03d720704f5705ae8502
Miguel Chichorro de Oliveira <51164882+MiguelChichorro@users.noreply.github.com>
2021-10-03 13:15:07 -0300

    Finish exe 9

--- Programming Language/list 2/Exercise 8.c
+++ Programming Language/list 2/Exercise 8.c
@@ -3,25 +3,24 @@
 #define RED "\x1b[31m"
 #define GREEN "\x1b[32m"
 #define RESET "\x1b[0m"
-int main(int argc, char** argv)
-{
- float a, b, c, x1, x2, delta;
- 
- printf("Digite o valor do termo a: ");
- scanf("%f", &a);
- printf("Digite o valor do termo b: ");
- scanf("%f", &b);
- printf("Digite o valor do termo c: ");
- scanf("%f", &c);
- delta = b*b - 4*a*c;
- x1 = (-b + sqrt(delta)) / (2*a);
- x2 = (-b - sqrt(delta)) / (2*a);
- if(delta < 0){
- printf(RED"A equacao nao possui raizes reais.n"RESET);
- } 
- else{
- printf(GREEN"O valor de x1: %.2fn",RESET, x1);
- printf(GREEN"O valor de x2: %.2fn",RESET, x2);
- }
- return 0;
+int main(int argc, char** argv){
+    float a, b, c, x1, x2, delta;
+    
+    printf("Digite o valor do termo a: ");
+    scanf("%f", &a);
+    printf("Digite o valor do termo b: ");
+    scanf("%f", &b);
+    printf("Digite o valor do termo c: ");
+    scanf("%f", &c);
+    delta = b*b - 4*a*c;
+    x1 = (-b + sqrt(delta)) / (2*a);
+    x2 = (-b - sqrt(delta)) / (2*a);
+    if(delta < 0){
+        printf(RED"A equacao nao possui raizes reais.n"RESET);
+    } 
+    else{
+        printf(GREEN"O valor de x1: %.2fn",RESET, x1);
+        printf(GREEN"O valor de x2: %.2fn",RESET, x2);
+    }
+    return 0;
 }
---
+++ Programming Language/list 2/Exercise 9.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void){
+	float salario;
+	printf("Insira o salario de um funcionario para calcular o seu Imposto de renda\n");
+	scanf("%f", &salario);
+	if (salario <= 1903.98) puts("\nIsento");
+	if (salario <= 1903.98 && salario <= 2826.65) puts("\nAliquota de 7.5%");
+	if (salario <= 2826.65 && salario <= 3751.05) puts("\nAliquota de 15.0%");
+	if (salario <= 3751.05 && salario <= 4664.68) puts("\nAliquota de 22.5%");
+	if (salario <= 4664.68) puts("\nAliquota de 27.5%");
+	return 0;
+}
