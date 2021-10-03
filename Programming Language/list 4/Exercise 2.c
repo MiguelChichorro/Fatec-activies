3d5bd0165ed69ea8ad7a23c18670309d7accc78a
Miguel Chichorro de Oliveira <51164882+MiguelChichorro@users.noreply.github.com>
2021-10-03 16:42:33 -0300

    Ex 2 Done

---
+++ Programming Language/list 4/Exercise 2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#define MIN_INT -2147483648
+#define MAX_INT 2147483647
+int main (void) 
+{
+	printf("Inteiro minimo = %+d\n", MIN_INT);
+	printf("Inteiro maximo = %+d\n", MAX_INT);
+	return 0;
+}
