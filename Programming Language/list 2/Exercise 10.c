820b9d1e4769984edd807b09d3e6529e9254ac04
Miguel Chichorro de Oliveira <51164882+MiguelChichorro@users.noreply.github.com>
2021-10-03 13:50:58 -0300

    Exercise 10 done

---
+++ Programming Language/list 2/Exercise 10.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void){
+	float x, y;
+	char o;
+	printf("Expressao? ");
+	scanf("%f %c %f", &x, &o, &y);
+	switch(o){
+		case '+':printf("Valor = %.2f\n", x+y); break;
+		case '-':printf("Valor = %.2f\n", x-y); break;
+		case '*':printf("Valor = %.2f\n", x*y); break;
+		case '/':if(y==0){
+			printf("Nao eh possivel dividir por zero\n");
+		}
+		else{
+			printf("Valor = %.2f\n", x/y); break;
+		}
+		default : printf("Operador invalido: %c\n", o);
+	}return 0;
+}
--- Programming Language/list 2/Exercise 9.c
+++ Programming Language/list 2/Exercise 9.c
@@ -6,9 +6,9 @@ int main(void){
 	printf("Insira o salario de um funcionario para calcular o seu Imposto de renda\n");
 	scanf("%f", &salario);
 	if (salario <= 1903.98) puts("\nIsento");
-	if (salario <= 1903.98 && salario <= 2826.65) puts("\nAliquota de 7.5%");
-	if (salario <= 2826.65 && salario <= 3751.05) puts("\nAliquota de 15.0%");
-	if (salario <= 3751.05 && salario <= 4664.68) puts("\nAliquota de 22.5%");
-	if (salario <= 4664.68) puts("\nAliquota de 27.5%");
+	else if (salario <= 1903.98 && salario <= 2826.65) puts("\nAliquota de 7.5%");
+	else if (salario <= 2826.65 && salario <= 3751.05) puts("\nAliquota de 15.0%");
+	else if (salario <= 3751.05 && salario <= 4664.68) puts("\nAliquota de 22.5%");
+	else if (salario <= 4664.68) puts("\nAliquota de 27.5%");
 	return 0;
 }
