77f98b7012b987a381b69508c35263031e1b96c7
Miguel Chichorro de Oliveira <51164882+MiguelChichorro@users.noreply.github.com>
2021-10-06 16:30:46 -0300

    Ex 6 Done

---
+++ Programming Language/list 5/Exercise 6.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int cr(int n){
+	if(n==-1)return 1;
+	printf("%d\n", n);
+	return n - cr(n-1);
+}
+
+int main (void){
+	int n;
+	printf("Num? ");
+	scanf("%d", &n);
+	cr(n);
+	return 0;
+}
