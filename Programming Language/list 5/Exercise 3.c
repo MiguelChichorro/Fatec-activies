ce67818c06cbfd9c4b4480b2ec7cd619b8ace70e
Miguel Chichorro de Oliveira <51164882+MiguelChichorro@users.noreply.github.com>
2021-10-06 16:27:54 -0300

    Ex 3 Done

---
+++ Programming Language/list 5/Exercise 3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int termial (int n){
+	if (n==1) return 1;
+	return n+termial(n-1);
+}
+
+int main (void){
+	int n;
+	printf("Num? ");
+	scanf("%d",&n);
+	printf("Termial = %d\n", termial(n));
+	return 0;
+}
