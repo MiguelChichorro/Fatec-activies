ecff42a34fd26c2d14b4e177c3beb4186ed3da4e
Miguel Chichorro de Oliveira <51164882+MiguelChichorro@users.noreply.github.com>
2021-10-06 15:51:19 -0300

    Ex 11 Done

---
+++ Programming Language/list 4/Exercise 11.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <conio.h>
+#include <time.h>
+
+void sequencia(void){
+	static int n=0;
+	printf("%d,", n++);	
+}
+
+int main (void){
+	while ( !_kbhit() ){
+		sequencia();
+		_sleep(1);
+	}
+	return 0;
+}
