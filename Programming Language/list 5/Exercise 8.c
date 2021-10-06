578b7cb959e5a8d767916fe57e8d44f6c5ddbf65
Miguel Chichorro de Oliveira <51164882+MiguelChichorro@users.noreply.github.com>
2021-10-06 16:32:49 -0300

    Ex 8 Done

---
+++ Programming Language/list 5/Exercise 8.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void hanoi(int n, char a, char b, char c){
+		if (n == 1)
+			printf("mova disco %d de %c para %c\n", n, a, b);
+		else{
+	    	hanoi(n - 1, a, c, b);
+			printf("Mova o disco %d de %c para %c\n", n, a, b);
+			hanoi(n - 1, c, b, a);
+			}
+}
+
+int main(void){
+int n;
+	printf("Insira a quantidade de discos: \n");
+	scanf("%d", &n);
+	hanoi(n, 'A', 'B', 'C');
+return 0;
+}
