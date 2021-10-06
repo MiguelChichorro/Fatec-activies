95ad1bb7a2aae0d20ae25f7f4bc3357ee35987bc
Miguel Chichorro de Oliveira <51164882+MiguelChichorro@users.noreply.github.com>
2021-10-06 16:33:57 -0300

    Ex 9 Done

---
+++ Programming Language/list 5/Exercise 9.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void){
+	int n;
+	printf("Digite um numero maior que zero: ");
+	scanf("%d",&n);
+	par(n);
+	return 0;
+}
+
+int par(int n){
+	if (n==1){
+		printf("O numero eh impar\n");
+		return 0;
+	}
+	else if (n==2){
+		printf("O numero eh par\n");
+		return 0;
+	}
+	par(n-2);
+}
