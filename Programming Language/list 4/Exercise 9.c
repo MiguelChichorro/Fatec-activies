86d8437dbb81644ffc1a4a2daa9c5d0278addcd8
Miguel Chichorro de Oliveira <51164882+MiguelChichorro@users.noreply.github.com>
2021-10-06 15:50:04 -0300

    Ex 9 Done

---
+++ Programming Language/list 4/Exercise 9.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void){
+	int n, fat;
+	printf("Fatorial do numero: ");
+	scanf("%d", &n);
+	printf("Fatorial: %d\n", fatorial(n, fat));
+return 0;
+}
+
+int fatorial (n, fat){
+	for(fat = 1; n > 1; n = n - 1)
+		fat = fat * n;
+	return fat;
+}
