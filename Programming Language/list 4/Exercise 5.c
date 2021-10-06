ee4059b0b984110bcbe1cd188529b0c1f1f79d71
Miguel Chichorro de Oliveira <51164882+MiguelChichorro@users.noreply.github.com>
2021-10-06 15:16:06 -0300

    Ex 5 Done

---
+++ Programming Language/list 4/Exercise 5.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<math.h>
+
+int Passo02(float r, float x){
+    if ((pow(r,2)-x)<0.001){
+        printf("%f \n", r);
+    }
+    else{
+        r = (pow(r,2)+x)/(2*r);
+        Passo02 (r,x);
+    }
+}
+
+int main (void) {
+	float x, r;
+	printf ("Valor de X ? \n");
+	scanf ("%f", &x);
+	r = x/2;
+	Passo02(r,x);
+	return 0;
+}
