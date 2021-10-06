b61b196fddebd30fbaa92c843c01dbf13ca33c85
Miguel Chichorro de Oliveira <51164882+MiguelChichorro@users.noreply.github.com>
2021-10-06 16:36:21 -0300

    Ex 12 Done

---
+++ Programming Language/list 5/Exercise 12.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int n, aux;
+    printf("Numero? ");
+    scanf("%d", &n);
+    for(int i=0; i<n; i++)
+		aux = fibonacci(i+1);		
+	printf("A n-esimo termo eh:%d\n", aux);
+}
+
+int fibonacci(int n)
+{
+   if(n==1 || n==2)
+       return 1;
+   else
+       return fibonacci(n-1) + fibonacci(n-2);
+}
